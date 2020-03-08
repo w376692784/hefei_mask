8b690535fd63fafc98ad20608c0ba521e5eb861e
w376692784 <376692784@qq.com>
2020-03-08 23:08:17 +0800

    v0.2

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ import execjs
 import json
 import re
 import time
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from multiprocessing import Pool
 
 class Mask():
-    def __init__(self):
+    def __init__(self, data, pharmacy):
+        print(data, pharmacy)
         self.payload = {
-            "name": "",
-            "cardNo": "",
-            "phone": "",
-            "pharmacyName": "百姓缘大药房金屯店",
-            "pharmacyCode": "1136",
+            "name": data[0],
+            "cardNo": data[1],
+            "phone": data[2],
+            "pharmacyName": pharmacy[0],
+            "pharmacyCode": pharmacy[1],
             "reservationNumber": "5",
             "hash": "",
             "timestamp": "",
@@ -22,42 +25,48 @@ class Mask():
         }
 
         self.headers = {
-                "Accept": "application/json, text/javascript, */*; q=0.01",
-                "Accept-Language": "zh-CN,zh;q=0.9",
-                "Connection": "keep-alive",
-                "Host": "kzgm.bbshjz.cn:8000",
-                "Content-Type": "application/json;charset=UTF-8",
-                "User-Agent": "Mozilla/5.0 (Linux; U; Android 2.3.6; zh-cn; GT-S5660 Build/GINGERBREAD) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1 MicroMessenger/4.5.255",
-                "User-Agent": "MicroMessenger",
-                # "X-Requested-With": "XMLHttpRequest",
-                "Cookie": ""
+            "Accept": "application/json, text/javascript, */*; q=0.01",
+            "Accept-Language": "zh-CN,zh;q=0.9",
+            "Connection": "keep-alive",
+            "Host": "kzgm.bbshjz.cn:8000",
+            "Content-Type": "application/json;charset=UTF-8",
+            "User-Agent": "Mozilla/5.0 (Linux; U; Android 2.3.6; zh-cn; GT-S5660 Build/GINGERBREAD) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1 MicroMessenger/4.5.255",
+            # "User-Agent": "MicroMessenger",
+            "X-Requested-With": "XMLHttpRequest",
+            "Cookie": ""
             }
         self.base_url = "http://kzgm.bbshjz.cn:8000/ncms"
 
     def crawl(self):
-        url = self.isv(self.base_url + "/mask/book")
-        params = self.payload
-        headers = self.headers
-        headers['Cookie'], params['captcha'] = self.captcha()
-        # cookies, params['captcha'] = self.captcha()
-        params['pharmacyPhaseName'], params['pharmacyPhase'] = self.get_last()
-        params['timestamp'] = str(int(time.time() * 1000) - 30000)
-        params['hash'] = self.md5(params['timestamp'])
+        count = 5
+        while count > 0:
+            url = self.isv(self.base_url + "/mask/book")
 
-        print("传递的参数为")
-        print(params)
-        response = requests.post(url=url, data=json.dumps(params), headers=headers)
-        print("获取的数据为")
-        print(response.json())
-        status = response.json()["status"]
-        if status == 200:
-            return True
-        else:
-            mes = response.json()['msg']
-            if "成功" in mes:
-                return True
+            self.payload['captcha'] = self.captcha()
+            self.payload['pharmacyPhaseName'], self.payload['pharmacyPhase'] = self.get_last()
+            self.payload['timestamp'] = str(int(time.time() * 1000) - 30000)
+            self.payload['hash'] = self.md5(self.payload['timestamp'])
+
+
+            print("传递的参数为")
+            print(self.payload)
+            response = requests.post(url=url, data=json.dumps(self.payload), headers=self.headers)
+            print("获取的数据为")
+            print(str(self.payload["name"]) + " " + str(response.json()))
+            status = response.json()["status"]
+            if status == 200:
+                print(self.payload["name"] + " " + self.payload["pharmacyName"] + " success")
+                return self.payload["name"] + " " + self.payload["pharmacyName"] + " success"
             else:
-                return False
+                mes = response.json()['msg']
+                if "成功" in mes:
+                    print(self.payload["name"] + " " + self.payload["pharmacyName"] + " success")
+                    return self.payload["name"] + " " + self.payload["pharmacyName"] + " success"
+                else:
+                    count -= 1
+                    time.sleep(1)
+        print(self.payload["name"] + " " + self.payload["pharmacyName"] + " failed")
+        return self.payload["name"] + " " + self.payload["pharmacyName"] + " failed"
 
         # print(captcha.text)
 
@@ -66,16 +75,16 @@ class Mask():
         captcha = requests.get(url=captcha_url, headers=self.headers)
         strings = captcha.text[:257] + 'E' + captcha.text[258:269] + 'E' + captcha.text[270:]
 
-        with open("captcha.jpg", "wb") as f:
+        with open(self.payload["name"] + " captcha.jpg", "wb") as f:
             f.write(base64.urlsafe_b64decode(strings))
-        while "Set-Cookie" not in captcha.headers:
-            print(captcha.headers)
-            captcha = requests.get(url=captcha_url, headers=self.headers)
-            time.sleep(3)
-        print(captcha.headers)
+        # print(captcha.headers)
         # return captcha.cookies, self.slove_captcha(strings)
-        return captcha.headers['Set-Cookie'].split(';')[0], self.slove_captcha(strings)
-
+        # return captcha.headers['Set-Cookie'].split(';')[0], self.slove_captcha(strings)
+        try:
+            self.headers['Cookie'] = captcha.headers['Set-Cookie'].split(';')[0]
+        except Exception as e:
+            pass
+        return self.slove_captcha(strings)
 
     def slove_captcha(self, img):
         params = {"image": img}
@@ -107,31 +116,30 @@ class Mask():
     def get_last(self):
         url = self.base_url + "/mask/pharmacy-stock?code=1136"
         response = requests.get(url=self.isv(url), headers=self.headers)
-        print(self.isv(url))
+        # print(self.isv(url))
         text = re.findall(r'"text":"(.*?)","value":"(.*?)"', response.json()['msg'])[1]
         return text
 
 if __name__ == '__main__':
-    mask = Mask()
 
-    currentTimeStamp = time.time()  # 获取当前时间戳
-    time_local = time.localtime(currentTimeStamp)  # 格式化时间戳为本地时间
-    time_YmdHMS = time.strftime("%Y%m%d_%H%M%S", time_local)  # 自定义时间格式
-    print('currentTimeStamp:', currentTimeStamp)
-    print('time_local:', time_local)
-    print('time_YmdHMS:', time_YmdHMS)
+    users = [
+             ]
 
-    status = mask.crawl()
-    if status is False:
+    pharmacies = [["百姓缘大药房金屯店", "1136"],
+                ["百姓缘大药房金屯二店", "11255"]]
 
-        currentTimeStamp = time.time()  # 获取当前时间戳
-        time_local = time.localtime(currentTimeStamp)  # 格式化时间戳为本地时间
-        time_YmdHMS = time.strftime("%Y%m%d_%H%M%S", time_local)  # 自定义时间格式
-        print('currentTimeStamp:', currentTimeStamp)
-        print('time_local:', time_local)
-        print('time_YmdHMS:', time_YmdHMS)
+    mask = [Mask(user, pharmacy) for user in users for pharmacy in pharmacies]
 
-        time.sleep(10)
-        status = mask.crawl()
-    else:
-        print("抢到了")
+    pool = Pool(len(users))
+    for i in range(len(users)):
+        pool.apply_async(mask[i].crawl)
+    pool.close()
+    pool.join()
+    print("all done")
+
+    # currentTimeStamp = time.time()  # 获取当前时间戳
+    # time_local = time.localtime(currentTimeStamp)  # 格式化时间戳为本地时间
+    # time_YmdHMS = time.strftime("%Y%m%d_%H%M%S", time_local)  # 自定义时间格式
+    # print('currentTimeStamp:', currentTimeStamp)
+    # print('time_local:', time_local)
+    # print('time_YmdHMS:', time_YmdHMS)
